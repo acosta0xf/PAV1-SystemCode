2392528108c54f33458fc10400356cc8e182db19
RodrigoDiaz17 <51763225+RodrigoDiaz17@users.noreply.github.com>
2020-10-08 18:32:26 -0300

    Update FrmConfirmacion.Designer.cs

--- src/SYSTEMCODE/Capa de Vista/FrmConfirmacion.Designer.cs
+++ src/SYSTEMCODE/Capa de Vista/FrmConfirmacion.Designer.cs
@@ -54,7 +54,7 @@
             this.btnSalir.FlatAppearance.BorderSize = 2;
             this.btnSalir.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(123)))), ((int)(((byte)(255)))));
             this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Font = new System.Drawing.Font("Eurostile ExtendedTwo", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSalir.ForeColor = System.Drawing.Color.White;
             this.btnSalir.Location = new System.Drawing.Point(191, 192);
             this.btnSalir.Name = "btnSalir";
@@ -72,7 +72,7 @@
             this.btnIngresar.FlatAppearance.BorderSize = 2;
             this.btnIngresar.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(123)))), ((int)(((byte)(255)))));
             this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Font = new System.Drawing.Font("Eurostile ExtendedTwo", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIngresar.ForeColor = System.Drawing.Color.White;
             this.btnIngresar.Location = new System.Drawing.Point(51, 192);
             this.btnIngresar.Name = "btnIngresar";
@@ -84,13 +84,12 @@
             // 
             // lblUsuario
             // 
-            this.lblUsuario.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Font = new System.Drawing.Font("Eurostile ExtendedTwo", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(13, 158);
             this.lblUsuario.Location = new System.Drawing.Point(69, 160);
             this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(241, 18);
+            this.lblUsuario.Size = new System.Drawing.Size(348, 20);
             this.lblUsuario.TabIndex = 10;
             this.lblUsuario.Text = "¿DESEAS CERRAR EL SISTEMA?";
             // 
