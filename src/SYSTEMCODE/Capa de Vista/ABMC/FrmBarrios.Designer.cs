89e2c4a451307b86ce55d9cec4027bc77571079a
RodrigoDiaz17 <51763225+RodrigoDiaz17@users.noreply.github.com>
2020-10-08 18:04:38 -0300

    Update FrmBarrios.Designer.cs

--- src/SYSTEMCODE/Capa de Vista/ABMC/FrmBarrios.Designer.cs
+++ src/SYSTEMCODE/Capa de Vista/ABMC/FrmBarrios.Designer.cs
@@ -166,7 +166,7 @@
             this.btnAgregar.FlatAppearance.BorderSize = 2;
             this.btnAgregar.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(123)))), ((int)(((byte)(255)))));
             this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Font = new System.Drawing.Font("Eurostile", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAgregar.ForeColor = System.Drawing.Color.White;
             this.btnAgregar.Location = new System.Drawing.Point(18, 283);
             this.btnAgregar.Name = "btnAgregar";
